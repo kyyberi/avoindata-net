339d2a5bfbb44d8afceb560efb9a36ed143f8162
Jarkko Moilanen <jarkko.moilanen@uta.fi>
2013-07-13 01:25:17 +0300

    Lisäsin ruby koodiesimerkin tagien id haun käsittelylle

---
+++ examples/ruby/tags/tag_id_search.rb
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# 
+# Esimerkki ruby koodin pätkä, jolla haetaan ja tuloste-
+# taan tagien nimet ja määrät avoindata.net palvelun API:n 
+# kautta. 
+#
+# Tekijä: Jarkko Moilanen (@kyyberi)
+# Päivämäärä: 11.7.2013
+# Lisenssi: http://creativecommons.org/licenses/by-sa/3.0/
+# 
+# -------------------------------------------------------
+
+# Tarvittavat gemit, asenna tarvittaessa tyyliin: 
+# "gem install json"
+require 'open-uri'
+require 'json'
+
+
+# 1. Ensin haetaan ID tagien listauksesta
+
+# URL mistä tag lista haetaan
+@murl = 'http://api.avoindata.net/tags'
+# hae JSON sisältö
+@json_contents  = open(@murl) {|f| f.read }
+# parsi JSON
+@content = JSON.parse(@json_contents)
+# ota toisen objektin id talteen
+@needed_id = @content['tags'][1]['wordid']
+
+# 2. haetaan kyseisen id:n omaavan tagin kysymykset
+
+# URL mistä tag lista haetaan
+@murl = 'http://api.avoindata.net/tags/id/' + @needed_id.to_s 
+# hae JSON sisältö
+@json_contents  = open(@murl) {|f| f.read }
+# parsi JSON
+@content = JSON.parse(@json_contents)
+
+# iteroi tietojoukko
+@content[@needed_id.to_s].each do |item|
+  ititle = item['title']
+  # tulosta tai mitä sitten haluatkaaan tehdä
+  puts ititle
+end
