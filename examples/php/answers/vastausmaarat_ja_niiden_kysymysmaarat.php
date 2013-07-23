938d2b43cc6577a3fb7d4e254e8b465f4fcf5006
Jarkko Moilanen <jarkko.moilanen@ossoil.com>
2013-07-23 18:02:46 +0300

    Lisäsin PHP esimerkin 'Vastausmäärät päivän tarkkuudella'

---
+++ examples/php/answers/vastausmaarat_ja_niiden_kysymysmaarat.php
@@ -0,0 +1,33 @@
+<?php
+/* #########################################
+   PHP esimerkki miten vastausmäärät päivän
+   tarkkuudella luetaan
+
+   Jarkko Moilanen
+   23.7.2013
+   Lisenssi: http://creativecommons.org/licenses/by-sa/3.0/
+
+*/
+
+
+$json_url = "http://api.avoindata.net/answers";
+$json = file_get_contents($json_url);
+
+$obj = json_decode($json);
+$answers = $obj->{"answers"};
+
+foreach ($answers as $day) {
+  echo "\n".$day->date.":";
+  echo $day->count;
+}
+
+/* käänteisessä järjestyksessä  */
+$reversed = array_reverse($answers);
+echo "\nJa sama käänteisessä järjestyksessä\n";
+
+foreach ($reversed as $day) {
+  echo "\n".$day->date.":";
+  echo $day->count;
+}
+
+?>
